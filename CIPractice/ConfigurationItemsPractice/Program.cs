b82fb36becff856e6f1b528280cc049b25e01d9b
Jose Arias <joseam2969@gmail.com>
2019-08-27 15:19:53 -0400

    Impact Analysis Added

--- CIPractice/ConfigurationItemsPractice/Program.cs
+++ CIPractice/ConfigurationItemsPractice/Program.cs
@@ -14,7 +14,8 @@ namespace ConfigurationItemsPractice
             Console.WriteLine("Opcion 1: Registrar un nuevo CI");
             Console.WriteLine("Opcion 2: Ver CIs registrados");
             Console.WriteLine("Opcion 3: Configurar dependencias");
-            Console.WriteLine("Opcion 4: Salir");
+			Console.WriteLine("Opcion 4: Modificar CI");
+            Console.WriteLine("Opcion 5: Salir");
             Console.Write("Inserte una de las siguientes opciones para continuar: ");
             string choosedOption = Console.ReadLine();
             option = int.Parse(choosedOption);
@@ -29,13 +30,111 @@ namespace ConfigurationItemsPractice
                 case 3:
                     ConfigureDependencies();
                     break;
-                case 4:
+				case 4:
+					ModifyCI();
+					break;
+                case 5:
                     ExitProgram();
                     break;
             }
         }
 
-        private static void ExitProgram()
+		private static void ModifyCI()
+		{
+			Console.Clear();
+			Console.WriteLine("Seleccione del siguiente menu el CI a modificar: ");
+			GetAllCIs();
+			Console.Write("CI Seleccionado: ");
+			int selectedCI = int.Parse(Console.ReadLine());
+			ConfigurationItem configurationItem = GetSpecificCI(selectedCI);
+			Console.WriteLine($"Item de Configuracion de nombre {configurationItem.Name} en la version {configurationItem.Version}");
+			Console.WriteLine("Desea deprecar o hacer upgrade a este CI?");
+			Console.WriteLine("1. Deprecar CI");
+			Console.WriteLine("2. Upgrade CI");
+			Console.Write("Opcion seleccionada: ");
+			int option = int.Parse(Console.ReadLine());
+			switch (option)
+			{
+				case 1:
+					CheckDeprecationImpact(configurationItem, selectedCI);
+					break;
+				case 2:
+					CheckUpgradeImpact(configurationItem);
+					break;
+			}
+		}
+
+		private static ConfigurationItem GetSpecificCI(int selectedCI)
+		{
+			ConfigurationItem configurationItem;
+			if (File.Exists("CI.txt"))
+			{
+				string[] registeredCis = File.ReadAllLines("CI.txt");
+				for (int i = 0; i < registeredCis.Length; i++)
+				{
+					if (i == selectedCI)
+					{
+						var Name = registeredCis[i].Split('|')[0];
+						var version = registeredCis[i].Split('|')[1];
+						configurationItem = new ConfigurationItem(Name, version);
+						return configurationItem;
+					}
+
+				}
+			}
+			return null;
+		}
+
+		private static void CheckUpgradeImpact(ConfigurationItem configurationItem)
+		{
+			Console.Clear();
+			Console.Write("Introduzca la version a la que desea actualizar este CI: ");
+			string newVersion = Console.ReadLine();
+			var newVersionArray = newVersion.Split('.');
+			var actualVersionArray = configurationItem.Version.Split('.');
+			int counter = 0;
+			for (int i = 0; i < newVersion.Length; i++)
+			{
+				if (newVersionArray[i] != actualVersionArray[i] && counter == 0)
+				{
+					Console.WriteLine($"El cambio al CI de nombre {configurationItem.Name} con numero de version actual" +
+						$" {configurationItem.Version} es de gran impacto porque es una actualizacion a gran escala.");
+					Console.WriteLine($"Se desea cambiar de {configurationItem.Version} a {newVersion}");
+					Console.ReadLine();
+					return;
+				}
+				else if(newVersionArray[i] != actualVersionArray[i] && counter == 1)
+				{
+					Console.WriteLine($"El cambio al CI de nombre {configurationItem.Name} con numero de version actual" +
+						$" {configurationItem.Version} es de  impacto medio porque es una actualizacion pequenia.");
+					Console.WriteLine($"Se desea cambiar de {configurationItem.Version} a {newVersion}");
+					Console.ReadLine();
+					return;
+				}
+				else if(newVersionArray[i] != actualVersionArray[i] && counter == 2)
+				{
+					Console.WriteLine($"El cambio al CI de nombre {configurationItem.Name} con numero de version actual" +
+						$" {configurationItem.Version} es de bajo impacto porque es una actualizacion de seguridad");
+					Console.WriteLine($"Se desea cambiar de {configurationItem.Version} a {newVersion}");
+					Console.ReadLine();
+					return;
+				}
+				counter++;
+
+			}
+		}
+
+		private static void CheckDeprecationImpact(ConfigurationItem configurationItem, int selectedCI)
+		{
+			Console.Clear();
+			Console.WriteLine($"En caso de deprecar el configuration item de nombre {configurationItem.Name} con numero de version" +
+				$" {configurationItem.Version} serian afectados" +
+				$" los siguientes programas: ");
+			GetDependencies(selectedCI);
+
+		}
+
+		private static void ExitProgram()
         {
 			Console.WriteLine("Gracias por utilizar nuestro programa. ");
         }
